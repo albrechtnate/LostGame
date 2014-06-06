ecafab6963551318436f9cbfd1d8e17fbfe0bed8
Nathan Albrecht <nathanielalbrect@gmail.com>
2014-06-06 14:17:34 -0400

    Began work on health system and smoke moster distance detection.

---
+++ Assets/health.js
@@ -0,0 +1,20 @@
+﻿var health : int = 10;
+
+function Start () {
+
+}
+
+var box : Transform;
+ 
+function Update () {
+ 
+ var dist : float = Vector3.Distance(box.position, transform.position);
+ Debug.Log(dist);
+ 
+ if(dist <= 10){
+  health -= 1;
+	yield WaitForSeconds (0.25);
+ }else{
+  box.light.enabled = false;
+ }
+}
